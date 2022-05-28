202b8caab1e2583066988f6cbb7f5d12d09d6c73
rkutkarska <ralena.dimitrova@gmail.com>
2022-05-28 22:46:37 +0300

    Add resources and static route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const app = express();
 const port = 5000;
 
-const app = express();
+app.use('/static', express.static());
+
 app.get('/', (req, res) => {
     res.send('Hi');
 });
 
-app.listen(5000, () => console.log(`Server is listening on port ${port}...`));
+app.listen(5000, () => console.log(`App is listening on port ${port}...`));
