d41e13ae24af020ba4f278a688d42fa20269d2c6
rkutkarska <ralena.dimitrova@gmail.com>
2022-06-03 18:40:01 +0300

    Add new rubic
    Add persistance

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
+const fs = require('fs/promises');
+const path = require('path');
+
+const cubes = require('../db.json');
 
 router.get('/create', (req, res) => {
     res.render('create');
+});
+
+router.post('/create', (req, res) => {
+    const cube = req.body;
+
+    if (cube.length < 2) {
+        res.status(400).send('Invalid request!');
+        return;
+    }
+
+    cubes.push(cube);
+    
+    fs.writeFile(path.resolve('src','db.json'), JSON.stringify(cubes, '', 4), {encoding: 'utf-8'})
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        })
+
+    // res.render('/');
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const port = 5000;
 
 app.use('/static', express.static('public'));
 
+app.use(express.urlencoded({extended: false}));
+
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
