a60ac884e5bd48e9cbb560e7025ccde91af65491
rkutkarska <ralena.dimitrova@gmail.com>
2022-06-04 10:34:10 +0300

    Add MongoDB like id to created cubes

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -4,13 +4,16 @@ const path = require('path');
 const cubes = require('../db.json');
 
 exports.save = (cube) => {
-    cubes.push(cube);
-    let textData = JSON.stringify(cubes, '', 4);
+    // MongoDB like ids
+    const ObjectId = (m = Math, d = Date, h = 16, s = s => m.floor(s).toString(h)) =>
+    s(d.now() / 1000) + ' '.repeat(h).replace(/./g, () => s(m.random() * h));
+    
+    cubes.push({id: ObjectId(), ...cube});
+    let textData = JSON.stringify(cubes, '', 4);    
 
     return fs.writeFile(path.resolve('src', 'db.json'), textData, { encoding: 'utf-8' });
 }
 
 exports.getOne = (cubeId) => {
-    console.log(cubes.filter(x => x.id == cubeId));
     return cubes.filter(x => x.id == cubeId)[0];
 };
