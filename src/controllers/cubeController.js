b94cae3ac86dd359ce459a66d1797a94cda742d0
rkutkarska <ralena.dimitrova@gmail.com>
2022-06-05 21:53:16 +0300

    Make a db relation between cube and accessory

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const cubeService = require('../services/cubeService');
-const acessoryService = require('../services/accessoryService');
+const accessoryService = require('../services/accessoryService');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -27,15 +27,22 @@ router.post('/create', (req, res) => {
 
 router.get('/details/:id', async (req, res) => {
     const cube = await cubeService.getOne(req.params.id).lean();
-    console.log(cube);
+
     res.render('details', { cube });
 });
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
     const cube = await cubeService.getOne(req.params.cubeId).lean();
-    const accessories = await acessoryService.getAll();
-    console.log(cube);
-    res.render('accessory/attach', {cube, accessories});
+    const accessories = await accessoryService.getAll();
+    res.render('accessory/attach', { cube, accessories });
 });
+
+router.post('/:cubeId/attach-accessory', async (req, res) => {
+    const accessoryId = req.body.accessory;
+
+    await cubeService.attachAccessory(req.params.cubeId, accessoryId);
+    res.redirect(`/cube/details/${req.params.cubeId}`);
+});
+
 
 module.exports = router;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -2,6 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const Cube = require('../models/Cube');
+const Accessory = require('../models/Accessory');
 
 exports.getAll = async (search, rawFrom, rawTo) => {
     let cubes = await Cube.find().lean();
@@ -11,3 +12,16 @@ exports.getAll = async (search, rawFrom, rawTo) => {
 exports.create = (cube) => Cube.create(cube);
 
 exports.getOne = (cubeId) => Cube.findById(cubeId);
+
+exports.attachAccessory = async (cubeId, accessoryId) => {
+    const cube = await Cube.findById(cubeId);
+    const accessory = await Accessory.findById(accessoryId);
+
+    cube.accessories.push(accessory);
+    accessory.cubes.push(cube);
+
+    await cube.save();
+    await accessory.save();
+
+    return cube;
+};
